58d667a3adfd9d667ba49a1a76e86bc02457001d
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-08 12:14:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -25,4 +25,10 @@ export default defineConfig({
       outDir: 'dist'
     }
   }
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
 })
+
