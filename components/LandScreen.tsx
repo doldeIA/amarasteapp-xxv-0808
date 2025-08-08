319bd86cc0ff2b06dfc3214f69a9ba7483e37d65
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-08 11:30:31 -0300

    Delete components directory 1

--- components/LandScreen.tsx
+++
@@ -1,9 +0,0 @@
-export default function LandingScreen() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <button className="bg-blue-600 text-white px-6 py-2 rounded">
-        Olá, Vercel!
-      </button>
-    </div>
-  )
-}
