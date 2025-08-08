a50994e1eabe0e1e5dc6a9cd04f22d81748517d5
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-08 11:55:54 -0300

    Commit directly to the main branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import path from 'path'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig({
+  plugins: [react()],
+})
   // Carrega as variáveis de ambiente do .env
   const env = loadEnv(mode, process.cwd(), '')
 
