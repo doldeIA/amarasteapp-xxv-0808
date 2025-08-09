2c512c2213227213bfd0f8d2ffa39cdfe6dc9880
Do Outro Lado do Espelho <contato@dolde.com.br>
2025-08-09 11:36:02 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import '../index.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>
-);
-
+const container = document.getElementById('root');
+if (container) {
+  createRoot(container).render(<App />);
+}
